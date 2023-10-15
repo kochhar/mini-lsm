f34c9224ff3269339ffb0893b38a2d96e20a7fa9
SK <sk@SKs-MacBook-Pro.local>
2023-10-15 20:51:09 +0530

    Implementation of merge iterators.

--- mini-lsm-starter/src/iterators/merge_iterator.rs
+++ mini-lsm-starter/src/iterators/merge_iterator.rs
@@ -1,8 +1,7 @@
-#![allow(unused_variables)] // TODO(you): remove this lint after implementing this mod
-#![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
-
 use std::cmp::{self};
 use std::collections::BinaryHeap;
+use std::collections::binary_heap::PeekMut;
+use std::debug_assert;
 
 use anyhow::Result;
 
@@ -39,29 +38,109 @@ impl<I: StorageIterator> Ord for HeapWrapper<I> {
 /// iterators, perfer the one with smaller index.
 pub struct MergeIterator<I: StorageIterator> {
     iters: BinaryHeap<HeapWrapper<I>>,
-    current: HeapWrapper<I>,
+    current: Option<HeapWrapper<I>>,
 }
 
 impl<I: StorageIterator> MergeIterator<I> {
     pub fn create(iters: Vec<Box<I>>) -> Self {
-        unimplemented!()
+        let mut heap = BinaryHeap::new();
+        let mut current = None;
+
+        for (idx, iter) in iters.into_iter().enumerate() {
+            if iter.is_valid() {
+                heap.push(HeapWrapper(idx, iter));
+            } else if heap.is_empty() {
+                // If all iters have been invalid, keep the last one as current
+                current = Some(HeapWrapper(0, iter));
+            }
+        }
+        // If at least 1 iter was valid, make the smallest one current
+        if !heap.is_empty() {
+            current = Some(heap.pop().unwrap());
+        }
+
+         Self {
+            iters: heap,
+            current: current,
+        }
     }
 }
 
 impl<I: StorageIterator> StorageIterator for MergeIterator<I> {
     fn key(&self) -> &[u8] {
-        unimplemented!()
+        assert!(self.is_valid(), "invalid iterator");
+        self.current.as_ref().unwrap().1.key()
     }
 
     fn value(&self) -> &[u8] {
-        unimplemented!()
+        assert!(self.is_valid(), "invalid iterator");
+        self.current.as_ref().unwrap().1.value()
     }
 
     fn is_valid(&self) -> bool {
-        unimplemented!()
+        match &self.current {
+            Some(w) => w.1.is_valid(),
+            _ => false
+        }
     }
 
     fn next(&mut self) -> Result<()> {
-        unimplemented!()
+        let current = self.current.as_mut().unwrap();
+
+        // Loop to advance past all the keys in the heap which are the
+        // same as the current key
+        while let Some(mut wrap_iter) = self.iters.peek_mut() {
+            debug_assert!(
+                wrap_iter.1.key() >= current.1.key(),
+                "min-heap invariant violaed"
+            );
+
+            if wrap_iter.1.key() == current.1.key() {
+                // If the key at the top of the heap is same as the current item
+                // then advance the heap iterator. This leads to one of three
+                // secnarios
+                // a. there is an error advancing -- remove from heap and return
+                // the error
+                if let e @ Err(_) = wrap_iter.1.next() {
+                    // PeekMut::pop pops the element without having to reorder
+                    // the heap again
+                    PeekMut::pop(wrap_iter);
+                    return e;
+                }
+
+                // b. the iterator terminates becoming invalid -- remove from
+                // heap and repeat the loop
+                if !wrap_iter.1.is_valid() {
+                    PeekMut::pop(wrap_iter);
+                }
+
+                // c. the iterator is valid -- repeat the loop
+            } else {
+                // If the key at the top of the heap is different from the current
+                // break out of the loop
+                break;
+            }
+        }
+
+        // Advance the current iterator. This leads to one of two scenarios
+        current.1.next()?;
+        // a. The current iterator becomes invalid. Replace it by popping
+        // from the top of the heap
+        if !current.1.is_valid() {
+            if let Some(wrap_iter) = self.iters.pop() {
+                *current = wrap_iter;
+            }
+            return Ok(());
+        }
+        // b. The current iterator remains valid. Compare it with the top of the
+        // heap and swap if current it bigger (sign used < because HeapWrapper
+        // comparisons are inverted to work with a max heap)
+        if let Some(mut wrap_iter) = self.iters.peek_mut() {
+            if *current < *wrap_iter {
+                std::mem::swap(&mut *wrap_iter, current);
+            }
+        }
+
+        Ok(())
     }
 }
--- mini-lsm-starter/src/iterators/two_merge_iterator.rs
+++ mini-lsm-starter/src/iterators/two_merge_iterator.rs
@@ -1,5 +1,4 @@
-#![allow(unused_variables)] // TODO(you): remove this lint after implementing this mod
-#![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
+use std::debug_assert;
 
 use anyhow::Result;
 
@@ -15,24 +14,60 @@ pub struct TwoMergeIterator<A: StorageIterator, B: StorageIterator> {
 
 impl<A: StorageIterator, B: StorageIterator> TwoMergeIterator<A, B> {
     pub fn create(a: A, b: B) -> Result<Self> {
-        unimplemented!()
+        Ok(Self {
+            a: a,
+            b: b,
+        })
+    }
+
+    fn prefer_a(&self) -> bool {
+        if self.a.is_valid() & self.b.is_valid() {
+            self.a.key() <= self.b.key()     
+        } else {
+            self.a.is_valid()
+        }
     }
 }
 
 impl<A: StorageIterator, B: StorageIterator> StorageIterator for TwoMergeIterator<A, B> {
     fn key(&self) -> &[u8] {
-        unimplemented!()
+        debug_assert!(self.is_valid(), "invalid iterator");
+        if self.prefer_a() {
+            &self.a.key()[..]
+        } else {
+            &self.b.key()[..]
+        }
     }
 
     fn value(&self) -> &[u8] {
-        unimplemented!()
+        debug_assert!(self.is_valid(), "invalid iterator");
+        if self.prefer_a() {
+            &self.a.value()[..]
+        } else {
+            &self.b.value()[..]
+        }
     }
 
     fn is_valid(&self) -> bool {
-        unimplemented!()
+        self.a.is_valid() || self.b.is_valid()
     }
 
     fn next(&mut self) -> Result<()> {
-        unimplemented!()
+        // both iterators cannot be invalid
+        debug_assert!(self.is_valid(), "invalid iterator");
+        if !self.b.is_valid() {
+            self.a.next()
+        } else if !self.a.is_valid() {
+            self.b.next()
+        } else {
+            if self.a.key() == self.b.key() {
+                self.a.next()?;
+                self.b.next()
+            } else if self.a.key() < self.b.key() {
+                self.a.next()
+            } else { 
+                self.b.next()
+            }
+        }
     }
 }
